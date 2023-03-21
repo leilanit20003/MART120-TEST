2afd4eda3966efe453da686c5d93819751f942a9
leilanit20003 <106109394+leilanit20003@users.noreply.github.com>
2023-03-20 18:36:56 -0600

    Add files via upload

---
+++ WK 9/sketch.js
@@ -0,0 +1,27 @@
+
+
+function setup() {
+    createCanvas(500,600);
+}
+
+    function draw() {
+        background(120, 45, 78);
+        textSize(32);
+        text('Hello there!', 10, 30);
+     }
+    
+    function draw() {
+        background(220);
+        circle(30,40,50);
+    }
+
+    function draw() {
+        backround(220);
+        square(30,40,50);
+    }
+
+    function draw() {
+        background(220);
+        ellipse(30,40,50,60);
+    
+    }
