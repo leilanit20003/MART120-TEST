e411b613a9342edc84f8fae2200de831d676008c
leilanit20003 <106109394+leilanit20003@users.noreply.github.com>
2023-04-25 13:56:49 -0600

    WK 9

--- WK 9/sketch.js
+++ WK 9/sketch.js
@@ -14,43 +14,36 @@ var x3 = 150;
 var y3 = 250;
 
 function setup() {
-    createCanvas(500,600);
+    createCanvas(500, 600);
     movement = Math.floor(Math.random() * 10) + 1;
 }
 
-    function draw()
-    {
-        background(220);
+function draw() {
+    console.log(frameCount)
+    background(220, 50, 120);
 
-        textSize(32);
-        text('Hello there!', 10, 30);
+    textSize(32);
+    text('Hello there!', 10, 30);
 
-        circle(50,70,50);
-        circle(x, y, 25);
-        if (x >= 500 || x <= 0) {
-            movement *= -1;
-        }
-        x += movement;
-        
-        function draw() {
-            background(220);
-            square(30,40,50);
-        }
-
-        function draw() {
-            background(220);
-            triangle(30, 75, 58, 20, 86, 75);
-        }
-
-        function draw() {
-            background(220);
-            line(30, 20, 85, 75);
-        }
-
-
-        function text()
-            textSize(32);
-            text('Michelle Lani Terry', 10, 30);
-        
-
+    circle(50, 70, 50);
+    circle(x, y, 25);
+    if (x >= 500 || x <= 0) {
+        movement *= -1;
     }
+    x += movement;
+
+    square(30, 40, 50);
+
+
+    triangle(30, 75, 58, 20, 86, 75);
+
+    line(130, 120, 85, 75);
+
+
+
+
+    textSize(32);
+    text('Michelle Lani Terry', 10, 30);
+
+
+}
